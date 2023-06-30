cbfe845323781877ec28b169ba2423f10cfc5730
Simon Pioli <simon@simonpioli.com>
2023-06-30 15:58:00 +0100

    We need to accept traffic from localhost too

--- src/Http/Middleware/LocalOnly.php
+++ src/Http/Middleware/LocalOnly.php
@@ -15,12 +15,21 @@ class LocalOnly
      */
     public function handle(Request $request, Closure $next): Response | JsonResponse | RedirectResponse
     {
+        $trusted = false;
+
         $domain = str_replace('https://', '', env('APP_URL'));
         if (env('APP_BASE_DOMAIN')) {
             $domain = env('APP_BASE_DOMAIN');
         }
 
-        if (!str_contains($request->headers->get('referer'), $domain)) {
+        if (str_contains($request->headers->get('referer'), $domain)) {
+            $trusted = true;
+        }
+        if (str_contains($request->headers->get('referer'), 'localhost')) {
+            $trusted = true;
+        }
+
+        if (!$trusted) {
             return response(['errorMessage' => 'Invalid origin', 'code' => 400], 400);
         }
 
